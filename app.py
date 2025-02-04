18ed069c14656b231ed6bee987524223c651bd20
lalu46614 <lalu46614@gmail.com>
2025-02-04 13:19:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -67,4 +67,4 @@ async def predict(file: UploadFile = File(...)):
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=10000)
-        return {"error": "Internal Server Error", "message": str(e)}
+        
